a0ce3b9d9221191b6a83ac68a8a772ab22fa9c76
Matvey <BarMatVla@gmail.com>
2023-12-26 21:57:03 +0300

    Небольшие добавления данных в <levels.py>; Создания окна выбора уровня (пока что без уровней) в <main.py>.

--- main.py
+++ main.py
@@ -41,8 +41,7 @@ class MainGame:
 
         button_play = Button((230, 280, 240, 100),
                              {0: "Играть", 1: "Play"}[self.language],
-                             is_bold=True, fsize=90, rounding=7,
-                             back_color=[153] * 3)
+                             is_bold=True, fsize=90, rounding=7, function=self.to_select_level)
 
         button_settings = Button((400, 20, 280, 80),
                                  {0: "Настройки", 1: "Settings"}[self.language],
@@ -61,11 +60,10 @@ class MainGame:
                 if e.type == pygame.QUIT:
                     exit()
                 if e.type == pygame.MOUSEBUTTONDOWN:
-                    button_guide.check_click(e.pos)
-                    button_settings.check_click(e.pos)
-            button_play.draw(self.w)
-            button_settings.draw(self.w)
-            button_guide.draw(self.w)
+                    [button.check_click(e.pos)
+                        for button in [button_play, button_settings, button_guide]]
+            [button.draw(self.w)
+                for button in [button_play, button_settings, button_guide]]
 
             pygame.display.update()
             clock.tick(FPS)
@@ -130,6 +128,45 @@ class MainGame:
             guide = GuideRu()
         guide.show()
         app.exec()
+
+    def to_select_level(self):
+        button_prev = Button([25, 380, 200, 60],
+                             {0: "Предыдущий Уровень", 1: "Previous Level"}[self.language],
+                             fsize={0: 23, 1: 31}[self.language],
+                             function=self.minus_id_level,
+                             rounding=12
+                             )
+        button_next = Button([475, 380, 200, 60],
+                             {0: "Следующий Уровень", 1: "Next Level"}[self.language],
+                             fsize={0: 26, 1: 41}[self.language],
+                             function=self.plus_id_level,
+                             rounding=12
+                             )
+        button_menu = Button([250, 375, 200, 70],
+                             {0: "В Меню", 1: "To Menu"}[self.language],
+                             fsize=60,
+                             rounding=14,
+                             function=self.to_menu)
+
+
+        while True:
+            self.w.fill((0, 0, 0))
+            for e in pygame.event.get():
+                if e.type == pygame.QUIT:
+                    exit()
+                if e.type == pygame.MOUSEBUTTONDOWN:
+                    [button.check_click(e.pos) for button in [button_prev, button_next, button_menu]]
+
+            [button.draw(self.w) for button in [button_prev, button_next, button_menu]]
+
+            pygame.display.update()
+            clock.tick(FPS)
+
+    def minus_id_level(self):
+        self.levelID = max([0, min([self.levelID - 1, len(levels_json) - 1])])
+
+    def plus_id_level(self):
+        self.levelID = max([0, min([self.levelID + 1, len(levels_json) - 1])])
 
 
 MainGame(window)
